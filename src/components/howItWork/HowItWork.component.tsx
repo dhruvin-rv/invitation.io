58d868dc9084b7cfcf248fca670fc37402ac11d6
WhiteStone-Mac1 <whitestone.mac.1@gmail.com>
2023-10-13 13:27:41 +0530

    hiw changes

--- src/components/howItWork/HowItWork.component.tsx
+++ src/components/howItWork/HowItWork.component.tsx
@@ -1,17 +1,20 @@
 import Image from "next/image";
 import React from "react";
-import SubHomeF1 from "../../../public/images/Subtract_home_f1.svg";
 import styles from "./hiw.module.css";
 import uploadCardSvg from "../../../public/images/upload_card_svg.svg";
 import uploadSheetSvg from "../../../public/images/upload_sheet_svg.svg";
 import selectAreaSvg from "../../../public/images/upload_sheet_svg.svg";
 import downloadPdfSvg from "../../../public/images/download_file_svg.svg";
+import { Chip, Divider } from "@mui/material";
 const HIW = () => {
   return (
     <>
-      <Image src={SubHomeF1} style={{ width: "100%" }} alt="blue shape" />
-      <div className={styles.hiw_main}>
+      <Divider style={{margin:"100px 0px 0px"}}>
+        <div className={styles["hiw-chip"]}>
         <h1>How It Works?</h1>
+        </div>
+      </Divider>
+      <div className={styles.hiw_main}>
         <p>
           Complete your card with the right paper finish on your choice of
           eco-friendly,high-quality paper stock.
@@ -54,10 +57,9 @@ const HIW = () => {
         <div className={`${styles.hiw_step} ${styles.hiw_item}`}>
           <h2>Download PDFs file </h2>
           <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry&apos;s standard dummy
-            text ever since the 1500s, when an unknown printer took a galley of
-            type and scrambled it to make a type specimen book.
+            One you select the area on the card, then click next where you can preview the samples of the Card according to your selection.
+            once you are satisfied with the preview, then click on the download button to download the PDF file. Where you have to select what should be the
+            names of files you download from the columns of CSV file.
           </p>
         </div>
         <div className={`${styles.hiw_step} ${styles.hiw_item}`}>
