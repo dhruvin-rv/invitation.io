1e26a847658fcb151519422f14b7884a81119640
WhiteStone-Mac1 <whitestone.mac.1@gmail.com>
2023-10-16 18:07:01 +0530

    icon not loading issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,9 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <UploadProvider>
