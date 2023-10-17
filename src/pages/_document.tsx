34b7b293a6cebac056261976512be49706e0fda5
WhiteStone-Mac1 <whitestone.mac.1@gmail.com>
2023-10-17 17:51:06 +0530

    analytics verification

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,18 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-0MZ6FPYMYK"></Script>
+        <Script id="google-analytics">{`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-0MZ6FPYMYK');`}
+        </Script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="icon" href="/favicon.ico" />
         <link
